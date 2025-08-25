1a15ebf3b23331dfc51420fae158f204225f7843
Alan Guzmán <alaguzman209@gmail.com>
2025-08-25 01:41:10 -0300

    prentrega lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 import NavBar from "./componentes/NavBar/NavBar"
 
 const App = () => {
   return (
-    <NavBar/>
+    <div>
+      <NavBar/>
+      <ItemListContainer saludo="Bienvenido"/>
+    </div>
+    
   )
 }
 
